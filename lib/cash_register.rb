e905f9666c69153763dc31414643b9b0b2b3c701
Moose Davis <moosedavis2011@gmail.com>
2020-09-20 22:05:26 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,6 @@ class CashRegister
   end 
   
   def add_item(title, price, quantity=1)
-    binding.pry
     @last_transaction = price * quantity 
     self.total += price * quantity 
     quantity.times do
